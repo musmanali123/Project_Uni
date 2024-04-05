461079e8a1642d90a0b9708360789bde7633ebf8
Mohammad Usman Ali <82979356+musmanali123@users.noreply.github.com>
2024-04-05 21:57:54 +0500

    Update parking-core.js
    Update code

--- js/parking-core.js
+++ js/parking-core.js
@@ -6,7 +6,7 @@ class Entry{
        // var now = new Date();
       //  var year = now.getFullYear();
       //  var day = now.getDay();
-        var month = now.getMonth();
+      //  var month = now.getMonth();
         var hours = now.getHours();
         var minutes = now.getMinutes();
         var sec = now.getSeconds();
